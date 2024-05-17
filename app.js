add53f31e00b52f183deb6e1a00d5597315fe996
raphael.monteiro <raphahenrique2808@gmail.com>
2024-05-17 14:54:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10: ');
+
+function verificarChute() {
+    console.log('O botão foi clicado!');
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
