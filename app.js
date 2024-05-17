b7f7209536ed0a0de42a1edc35e27b8d9919bcff
raphael.monteiro <raphahenrique2808@gmail.com>
2024-05-17 14:57:00 -0300

    Projeto 2 - Alura

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10: ');
 
 function verificarChute() {
+    let chute = documemt.querySelector('input').value;
     console.log('O botão foi clicado!');
 }
 
